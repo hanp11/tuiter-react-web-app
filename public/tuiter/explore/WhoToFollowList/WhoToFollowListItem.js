9e4d13483f352d6eaf830bcbdf42df7084ceff1a
Phyu Han <phyuhan77@gmail.com>
2022-10-15 00:06:35 -0400

    adds a5 tuiter

---
+++ public/tuiter/explore/WhoToFollowList/WhoToFollowListItem.js
@@ -0,0 +1,25 @@
+const WhoToFollowListItem = (who) => {
+    return (`
+        <li class="list-group-item">
+            <div class="row align-items-center">
+                <div class="col-2 col-xl-auto">
+                    <img class="wd-follow-img rounded-circle"
+                         src=${who.avatarIcon}/>
+                </div>
+                <div class="col-6 col-xl">
+                    <div class="fw-bolder text-nowrap">
+                        ${who.userName}
+                        <i class="fa-solid fa-circle"></i>
+                    </div>
+                    <div>@${who.handle}</div>
+                </div>
+                <div class="col-4 col-xl-auto">
+                    <button class="btn btn-primary btn-block rounded-pill float-end">
+                        Follow
+                    </button>
+                </div>
+            </div>
+        </li>
+    `);
+}
+export default WhoToFollowListItem;
---
+++ public/tuiter/explore/WhoToFollowList/who.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        avatarIcon: '/public/images/java.jpg',
+        userName: 'Java',
+        handle: 'Java',
+    },
+    {
+        avatarIcon: '/public/images/relativity-space.jpg',
+        userName: 'Relativity Space',
+        handle: 'relativityspace',
+    },
+    {
+        avatarIcon: '/public/images/virgin-galactic.jpg',
+        userName: 'Virgin Galactic',
+        handle: 'virgingalactic',
+    },
+    {
+        avatarIcon: '/public/images/nasa.jpg',
+        userName: 'NASA',
+        handle: 'NASA',
+    },
+    {
+        avatarIcon: '/public/images/tesla.jpg',
+        userName: 'Tesla',
+        handle: 'Tesla',
+    },
+];
