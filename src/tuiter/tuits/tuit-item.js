e63cfbbaee12d633aa0caa7cea49ef2c9ace8167
Phyu Han <phyuhan77@gmail.com>
2022-11-18 12:50:16 -0500

    Adds dislikes

--- src/tuiter/tuits/tuit-item.js
+++ src/tuiter/tuits/tuit-item.js
@@ -15,15 +15,18 @@ const TuitItem = ({tuit}) => {
       <li className="list-group-item">
         <div className="row">
           <div className="col-auto">
-            <img className="wd-small-img rounded-circle" src={`/images/${tuit.image}`}/>
+            <img className="wd-small-img rounded-circle"
+                 src={tuit.image ? `/images/${tuit.image}` : '/images/nasa.jpg'}/>
           </div>
           <div className="col">
             <i className="bi bi-x-lg float-end"
                onClick={() => deleteTuitHandler(tuit._id)}></i>
             <span className="fw-bolder">{tuit.userName}</span>
-            <i className="bi bi-patch-check-fill text-primary p-1"></i>
-            <span className="text-secondary pe-1">{tuit.handle}</span>
-            <span className="text-secondary">&bull; {tuit.time}</span>
+            {
+              //<i className="bi bi-patch-check-fill text-primary p-1"></i>
+            }
+            <span className="text-secondary pe-1">{tuit.handle ? tuit.handle : '@NASA'}</span>
+            <span className="text-secondary">&bull; {tuit.time ? tuit.time : '0h'}</span>
             <div>{tuit.tuit}</div>
             <TuitStats key={tuit._id} tuit={tuit}/>
           </div>
--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -6,19 +6,25 @@ const TuitStats = ({tuit}) => {
   const dispatch = useDispatch();
   return (
       <div className="row mt-2 mb-2">
-        <div className="col-3">
+        <div className="col-2">
           <i className="bi bi-chat pe-1"></i>
-          {tuit.replies}
+          {tuit.replies ? tuit.replies : 0}
         </div>
-        <div className="col-3">
+        <div className="col-2">
           <i className="bi bi-arrow-repeat pe-1"></i>
-          {tuit.retuits}
+          {tuit.retuits ? tuit.retuits : 0}
         </div>
         <div className="col-3">
-          <i onClick={() => dispatch(updateTuitThunk({...tuit, likes: tuit.likes + 1}))} className="bi bi-heart-fill me-2 text-danger"></i>
+          <i onClick={() => dispatch(updateTuitThunk({...tuit, likes: tuit.likes + 1}))}
+             className="bi bi-heart-fill me-2 text-danger"></i>
           {tuit.likes}
         </div>
         <div className="col-3">
+          <i onClick={() => dispatch(updateTuitThunk({...tuit, dislikes: tuit.dislikes + 1}))}
+             className="bi bi-hand-thumbs-down-fill me-2 text-danger"></i>
+          {tuit.dislikes ? tuit.dislikes : 0}
+        </div>
+        <div className="col-2">
           <i className="bi bi-share"></i>
         </div>
       </div>
